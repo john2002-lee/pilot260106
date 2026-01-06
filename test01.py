fc0fc7abee3773675c5b1c6f12e9000bab2387a9
maxbeny999 <maxbeny999@gmail.com>
2026-01-06 13:34:14 +0900

    4th

--- test01.py
+++ test01.py
@@ -7,3 +7,5 @@ print("what did u do last night?")
 print("i listened to rock musics and studied basic python")
 
 print("really? do u like heavymetal?")
+
+print("yse i do, heavymetal, rock and roll, progressive rock, disco rock i listen to all most all of them. my fav band is queen")
